9a40ddc9cb0918a062b2c58a8aa20b0fac9f2b63
Jan Schulte <j.schulte@52north.org>
2021-01-27 11:32:21 +0100

    add translations of old implementation with transformation script

---
+++ i18n/script.js
@@ -0,0 +1,50 @@
+'use strict';
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+
+const result = excelToJson({
+  sourceFile: 'i18n.xlsx',
+  header: {
+    rows: 1
+  },
+  columnToKey: {
+    A: 'context',
+    B: 'en',
+    C: 'de',
+    D: 'es',
+    E: 'fr',
+    F: 'it',
+    G: 'pl',
+    H: 'nl',
+    I: 'sk',
+    J: 'sv',
+    K: 'hr',
+    L: 'hu',
+    M: 'pt',
+    N: 'cz',
+    O: 'fi',
+    P: 'ro',
+    Q: 'bg',
+    R: 'el',
+    S: 'mt',
+    T: 'da',
+    U: 'et',
+    V: 'ga',
+    W: 'lt',
+    X: 'lv',
+    Y: 'sl',
+    Z: 'no'
+  }
+});
+
+const langs = ['en', 'de', 'es', 'fr', 'it', 'pl', 'nl', 'sk', 'sv', 'hr', 'hu', 'pt', 'cz', 'fi', 'ro', 'bg', 'el', 'mt', 'da', 'et', 'ga', 'lt', 'lv', 'sl', 'no'];
+
+langs.forEach(lang => {
+//   const lang = 'en';
+  const translation = {};
+  result.Sheet1.forEach(row => {
+    translation[row.context] = row[lang] || '';
+  })
+  const langstr = JSON.stringify(translation, null, 2);
+  fs.writeFile(`../src/assets/i18n/${lang}.json`, langstr, 'utf8', () => console.log(`wrote file for ${lang}`))
+})
