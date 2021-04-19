bfc1462d23404b304f494228e88f1fdf5f77c843
Jan Schulte <j.schulte@52north.org>
2021-04-19 11:27:15 +0200

    adjust naming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('geo-viewer app is running!');
+    expect(await page.getTitleText()).toEqual('deu-viewer app is running!');
   });
 
   afterEach(async () => {
